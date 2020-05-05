6532ca311593580aef581352cdf59ad87a38b55e
Mike Gretton <mikegretton@gmail.com>
2020-05-05 20:15:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
-function addElementToBeginningOfArray(array, element) {
+function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 

1ec9601e8be1cae467c92a8878b0283d26e15116
Mike Gretton <mikegretton@gmail.com>
2020-05-05 20:15:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 function addElementToBeginningOfArray(array, element) {
-  return [...array,. element]
+  return [...array, element]
 }
 
 let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
